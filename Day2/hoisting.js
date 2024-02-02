75e7d693875a210322d640cd66ea16e62ae8af7c
Sivakanta <125561206+Sivakanta@users.noreply.github.com>
2024-02-02 21:31:59 +0530

    Add files via upload

---
+++ Day2/hoisting.js
@@ -0,0 +1,21 @@
+getName2();
+getName();
+console.log(getName);
+console.log(x);
+
+x=7;
+
+function getName() {
+    console.log("Hello Javascript");
+}
+
+function getName2(){
+    console.log("Hello Javascript");
+}
+
+
+
+
+
+
+
---
+++ Day2/scope.js
@@ -0,0 +1,21 @@
+var x=1;
+a();
+b();
+console.log(x);
+
+function a(){
+    var x=10;
+    console.log(x);
+}
+
+function b(){
+    var x=100;
+    console.log(x);
+}
+
+if (true){
+    let x=1000;
+    console.log(x);
+}
+
+console.log(x);
